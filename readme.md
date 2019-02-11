920defe214653c7ac9de74c3b821d97fba882294
Jose Antonio Baena Bueno <joseantoniobaenabueno@gmail.com>
2019-02-11 22:28:14 +0100

    final version 1.04

---
+++ readme.md
@@ -0,0 +1,10 @@
+The goal of this project is creating bar graphs with gradient colors.
+
+We will use the average temperature of Malaga for months during 2018 as data.
+
+For this the only thing that we will have to do is to clone this repository, 
+and after that execute the command "npm start" to use node (they must download it) 
+and thus represent the graph in a localhost.
+
+In addition I have left as an option to paint the X axis with ".scaletime ()",
+ in case you need to use it or if you just prefer it that way instead of ".scaleband ()".
--- src/d3/barplot.d3.ts
+++ src/d3/barplot.d3.ts
@@ -5,31 +5,33 @@ import { axisBottom, axisLeft } from "d3-axis";
 import { max, min } from "d3-array";
 
 
-
+// We import all packages, including those from the Malaga temperature data.
 const d3 = {
   select, selectAll, scaleBand, scaleLinear, scaleTime, axisBottom, axisLeft, max, min,
 };
 
+//We create and initialize the variables for the card and the width of our bars.
 const width = 500;
 const height = 300;
 const padding = 50;
 const barWidth = 35;
 
 
-// Creamos la tarjeta.
+// We create the card.
 const card = select("#root")
   .append("div")
   .attr("class", "card");
 
-// Creamos el 'lienzo' svg.
+// We create the panel svg.
 const svg = card
   .append("svg")
   .attr("width", "100%")
   .attr("height", "100%")
   .attr("viewBox", `${-padding} ${-padding} ${width + 2 * padding} ${height + 2 * padding}`);
 
+// We create the scales. In addition we have commented on another possible scale ".scaletime ()" for the X axis, in case it is of your liking.
 const scaleYPos = d3.scaleLinear()
-  .domain([d3.min(avgTemp) - 5, d3.max(avgTemp) + 5])
+  .domain([d3.min(avgTemp) - 5, d3.max(avgTemp) + 5]) // I have the "- + 5" because this way we get more margin and there is no value in the X axis (empty bar) and no bar reaches the upper limit.
   .range([height, 0]);
 
 const scaleXPos = d3.scaleBand()
@@ -38,25 +40,27 @@ const scaleXPos = d3.scaleBand()
 
 /*const xScaleTime = d3 // If i wanted to do with ScaleTime
 .scaleTime()
-.domain([new Date(2018, -1), new Date(2018, 11)]) // Range Jan to Dec 2019
+.domain([new Date(2018, -1), new Date(2018, 11)]) // Range Jan to Dec 2018
 .range([0, width]); // pixels*/
 
+// Now we create the bars according to the previous scales.
 const barGroup = svg
   .append('g');
 
 barGroup
-  .selectAll('rect')
+  .selectAll('rect') // 'rect' for create bars.
   .data(avgTemp)
   .enter()
   .append("rect")
-  .attr("x", (d, i) => i * (width / 12) + 3)
+  .attr("x", (d, i) => i * (width / 12) + 3) // This has been done in order to distribute the space between the number of bars that will be available. The "+3" is so that there is some separation with the Y axis.
   .attr("y", d => scaleYPos(d))
   .attr("width", barWidth)
   .attr("height", d => height - scaleYPos(d))
-  .attr("fill", "url(#barGradient)");
+  .attr("fill", "url(#barGradient)"); // The colors will be selected below.
 
 
 const axisGroup = svg.append("g");
+
 // Y Axis: call axisLeft helper and pass the scale
 axisGroup.append("g").call(d3.axisLeft(scaleYPos));
 
